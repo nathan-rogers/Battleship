5f88169a43389590bacb84c1af1989d4b1e4d6ff
nathan-rogers <nathan.rogers.d@gmail.com>
2015-05-05 17:59:32 -0600

    Added randomization functionality

--- Battleship/Battleship/Program.cs
+++ Battleship/Battleship/Program.cs
@@ -12,6 +12,7 @@ namespace Battleship
         {
             Grid grid = new Grid();
             grid.PlayGame();
+            Console.ReadKey();
         }
 
     }
@@ -122,8 +123,8 @@ namespace Battleship
         //face the ship being placed
         public enum PlaceShipDirection
         {
-            Horizontal,
-            Vertical
+            Vertical,
+            Horizontal
         }
         //create multi-dimensional array for display and storage of point types
         public Point[,] Ocean { get; set; }
@@ -166,11 +167,77 @@ namespace Battleship
                 new Ship(Ship.ShipType.MilleniumFalcon),
                 new Ship(Ship.ShipType.StarCruiser)
             };
+            Random rng = new Random();
+            int placeX = 0;
+            int placeY = 0;
             for (int i = 0; i < ListOfShips.Count(); i++)
             {
-                for (int j = 0; j < ListOfShips[i].Length; j++)
+
+                int rotation = 0;
+                //while a direction has not been selected
+                while (rotation == 0)
                 {
-                    PlaceShip(ListOfShips[i], PlaceShipDirection.Horizontal, i, j);
+                    //select starting x coord
+                    placeX = rng.Next(0, 11);
+                    //starting y coord
+                    placeY = rng.Next(0, 11);
+                    //horizontal or vertical
+                    rotation = rng.Next(1, 2);
+                    
+                    switch (rotation)
+                    {   //vertical
+                        case 1:
+                            //while starting X + ship length DOES NOT fit on the board
+                            while (!((placeX + ListOfShips[i].Length) < 10)) 
+                            {
+                                //try a new x coord
+                                placeX = rng.Next(0, 11);
+                                
+                            }
+                            //if it fits on the board
+                            if ((placeX + ListOfShips[i].Length) < 10)
+                            {
+                                //make sure ships do not intersect
+                                for (int j = 0; j < ListOfShips[i].Length; j++)
+                                {
+                                    //if they do intersect
+                                    if (Ocean[placeX + j, placeY].Status == Point.PointStatus.Ship)
+                                    {
+                                        //start randomization section over
+                                        rotation = 0;
+                                        break;
+                                    }
+                                }
+                            }
+                            //if it passes everything, place a ship calling PlaceShip with randomized numbers
+                            PlaceShip(ListOfShips[i], (PlaceShipDirection)rotation, placeX, placeY);
+                            break;
+
+                        case 2:
+                            while (!((placeY + ListOfShips[i].Length) < 10))
+                            {
+                                placeY = rng.Next(0, 11);
+                                
+                            }
+                            if ((placeX + ListOfShips[i].Length) < 10)
+                            {
+                                for (int j = 0; j < ListOfShips[i].Length; j++)
+                                {
+                                    if (Ocean[placeX, placeY + j].Status == Point.PointStatus.Ship)
+                                    {
+                                        rotation = 0;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            PlaceShip(ListOfShips[i], (PlaceShipDirection)rotation, placeX, placeY);
+                            break;
+                        default:
+                            rotation = 0;
+                            break;
+
+                    }
                 }
             }
 
@@ -179,15 +246,20 @@ namespace Battleship
         public void PlaceShip(Ship placeShip, PlaceShipDirection direction, int startX, int startY)
         {
 
-            Ocean[startX, startY].Status = Point.PointStatus.Ship;
-            placeShip.OccupiedPoints.Add(Ocean[startX, startY]);
-            if (direction == PlaceShipDirection.Horizontal)
+            for (int i = 0; i < placeShip.Length; i++)
             {
-                startX++;
-            }
-            else if (direction == PlaceShipDirection.Vertical)
-            {
-                startY++;
+
+
+                Ocean[startX, startY].Status = Point.PointStatus.Ship;
+                placeShip.OccupiedPoints.Add(Ocean[startX, startY]);
+                if (direction == PlaceShipDirection.Horizontal)
+                {
+                    startX++;
+                }
+                else if (direction == PlaceShipDirection.Vertical)
+                {
+                    startY++;
+                }
             }
 
         }
@@ -218,22 +290,23 @@ namespace Battleship
                     }
                 }
                 //print grid numbers
+                Console.Write("X");
                 Console.Write(i + 1);
                 Console.WriteLine();
             }
-            Console.WriteLine(" 1  2  3  4  5  6  7  8  9  10");
-            Console.WriteLine("Ships Destroyed: {0}", ListOfShips.Count(x=>x.IsDestroyed == true));
+            Console.WriteLine("Y1 Y2 Y3 Y4 Y5 Y6 Y7 Y8 Y9 Y10 ");
+            Console.WriteLine("Ships Destroyed: {0}", ListOfShips.Count(x => x.IsDestroyed == true));
 
         }
         public bool Target(int x, int y)
         {
             // get count before move
-            int destroyedShipsBefore = ListOfShips.Count(ship=>ship.IsDestroyed == true);
-            
+            int destroyedShipsBefore = ListOfShips.Count(ship => ship.IsDestroyed == true);
+
             if (Ocean[x, y].Status == Point.PointStatus.Ship)
             {
                 Ocean[x, y].Status = Point.PointStatus.Hit;
-                
+
                 //destroyedShipAfter = ListOfShips.Where(a => a.IsDestroyed == true).Count();
 
             }
@@ -253,17 +326,40 @@ namespace Battleship
         }
         public void PlayGame()
         {
-            int xCoord = 1;
-            int yCoord = 1;
             while (ListOfShips.Where(x => x.IsDestroyed == true).Count() != 5)
             {
 
+                int xCoord = 0;
+                int yCoord = 0;
                 DisplayOcean();
-                Console.WriteLine("Please enter an x coordinate 1- 10: ");
-                int.TryParse(Console.ReadLine(), out xCoord);
+                while (xCoord == 0)
+                {
+
+                    Console.WriteLine("Please enter an x coordinate 1 - 10: ");
+                    int.TryParse(Console.ReadLine(), out xCoord);
+                    if (xCoord == 0)
+                    {
+                        Console.Clear();
+                        DisplayOcean();
+                        Console.WriteLine("Please enter a legitimate coordinate!");
+                    }
+                }
+
                 xCoord -= 1;
-                Console.WriteLine("Please enter a y coordinate 1-10: ");
-                int.TryParse(Console.ReadLine(), out yCoord);
+                while (yCoord == 0)
+                {
+
+                    Console.WriteLine("Please enter a y coordinate 1 - 10: ");
+                    int.TryParse(Console.ReadLine(), out yCoord);
+                    if (yCoord == 0)
+                    {
+
+                        Console.Clear();
+                        DisplayOcean();
+                        Console.WriteLine("Please enter a legitimate coordinate!");
+                    }
+                }
+
                 yCoord -= 1;
                 Target(xCoord, yCoord);
                 Console.Clear();
